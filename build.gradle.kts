b54b0cf5102f5fcf703fd6f0d5515402f536c4ac
watspartacus-sys <watspartacus@gmail.com>
2026-09-20 12:38:27 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+lplugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
