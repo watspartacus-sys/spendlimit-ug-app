9e7b13798a314f745d9f210dc0c31b29392e7aa8
watspartacus-sys <watspartacus@gmail.com>
2026-09-20 12:21:57 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,4 @@
-plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.devtools.ksp)
-}
+
 
 android {
     namespace = "com.aistudio.ugandaspendlimit.qvrtm"
