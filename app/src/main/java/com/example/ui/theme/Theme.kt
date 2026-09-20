a3c209f82193814b005fd1dfa93cedd5c2668ecc
watspartacus-sys <watspartacus@gmail.com>
2026-09-20 19:32:39 +0300

    Update Theme.kt

--- app/src/main/java/com/example/ui/theme/Theme.kt
+++ app/src/main/java/com/example/ui/theme/Theme.kt
@@ -10,6 +10,9 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 
 private val DarkColorScheme = darkColorScheme(
     primary = Color(0xFF6200EE),
@@ -21,6 +24,10 @@ private val LightColorScheme = lightColorScheme(
     primary = Color(0xFF6200EE),
     secondary = Color(0xFF03DAC6),
     tertiary = Color(0xFF3700B3)
+)
+
+val Typography = Typography(
+    bodyLarge = TextStyle(fontSize = 16.sp)
 )
 
 @Composable
@@ -37,7 +44,6 @@ fun SpendLimitUGTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
