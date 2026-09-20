fafcd3ecda51e1b456adb19a1da48e3173062e6f
watspartacus-sys <watspartacus@gmail.com>
2026-09-20 20:47:00 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,3 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-rootProject.name = "SpendLimit UG"
-include(":app")
