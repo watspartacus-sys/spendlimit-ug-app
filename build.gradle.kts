97b9e8e7e710090fcb4ce970a8aae47cc7add8e9
watspartacus-sys <watspartacus@gmail.com>
2026-09-20 11:44:39 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.devtools.ksp) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)   <- DELETE THIS LINE
+    alias(libs.plugins.devtools.ksp)
 }
