f47000341bf36bc5f36b6c7cb7b4ce602e97042d
watspartacus-sys <watspartacus@gmail.com>
2026-09-20 19:56:24 +0300

    Update Theme.kt

--- app/src/main/java/com/example/ui/theme/Theme.kt
+++ app/src/main/java/com/example/ui/theme/Theme.kt
@@ -1,52 +1,7 @@
 package com.example.ui.theme
-
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.sp
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Color(0xFF6200EE),
-    secondary = Color(0xFF03DAC6),
-    tertiary = Color(0xFF3700B3)
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Color(0xFF6200EE),
-    secondary = Color(0xFF03DAC6),
-    tertiary = Color(0xFF3700B3)
-)
-
-val Typography = Typography(
-    bodyLarge = TextStyle(fontSize = 16.sp)
-)
-
 @Composable
-fun SpendLimitUGTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = false,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+fun SpendLimitUGTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
 }
